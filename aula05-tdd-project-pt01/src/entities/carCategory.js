1abd5808bf59b28e8193964db6c589344fe81223
otaviopetry <otaviopetry@gmail.com>
2023-01-02 09:39:45 -0300

    feat: TDD & BDD project - part 1

---
+++ aula05-tdd-project-pt01/src/entities/carCategory.js
@@ -0,0 +1,17 @@
+const Base = require('./base/base');
+
+class CarCategory extends Base {
+    constructor({
+        id,
+        name,
+        carIds,
+        price,
+    }) {
+        super({ id, name });
+
+        this.carIds = carIds;
+        this.price = price;
+    }
+}
+
+module.exports = CarCategory;
