609cf56167c31a5c54cf98768cc4b977b52e2c2b
otaviopetry <otaviopetry@gmail.com>
2023-07-11 00:08:15 -0300

    refactor: test mother object

---
+++ modulo07/creational-patterns/03.test-data-builder-mother-object/test/productValidation-mother-object.test.js
@@ -0,0 +1,68 @@
+const { expect } = require('chai');
+const { it, describe } = require('mocha');
+const { productValidator } = require('../src');
+const ProductMotherObject = require('./model/productMotherObject');
+
+describe('Test Mother Object', () => {
+    it('shouldn\'t return error when product is valid', () => {
+        const product = ProductMotherObject.valid();
+        const result = productValidator(product);
+
+        const expected = {
+            errors: [],
+            result: true,
+        };
+
+        expect(result).to.be.deep.equal(expected);
+    });
+    
+    describe('Product Validation Rules', () => {
+        it('should return an object error when creating a Product with invalid id', () => {
+            const product = ProductMotherObject.withInvalidId();
+            const result = productValidator(product);
+
+            const expected = {
+                errors: ['id: invalid length, current [1] expected to be between 2 and 20'],
+                result: false,
+            };
+
+            expect(result).to.be.deep.equal(expected);
+        });
+
+        it('should return an object error when creating a Product with invalid name', () => {
+            const product = ProductMotherObject.withInvalidName();
+            const result = productValidator(product);
+
+            const expected = {
+                errors: ['name: invalid value, current [abc123] expected to be only words'],
+                result: false,
+            };
+
+            expect(result).to.be.deep.equal(expected);
+        });
+
+        it('should return an object error when creating a Product with invalid price', () => {
+            const product = ProductMotherObject.withInvalidPrice();
+            const result = productValidator(product);
+
+            const expected = {
+                errors: ['price: invalid value, current [2000] expected to be between 0 and 1000'],
+                result: false,
+            };
+
+            expect(result).to.be.deep.equal(expected);
+        });
+
+        it('should return an object error when creating a Product with invalid category', () => {
+            const product = ProductMotherObject.withInvalidCategory();
+            const result = productValidator(product);
+
+            const expected = {
+                errors: ['category: invalid value, current [synthethic] expected to be either eletronic or organic'],
+                result: false,
+            };
+
+            expect(result).to.be.deep.equal(expected);
+        });
+    });   
+});
