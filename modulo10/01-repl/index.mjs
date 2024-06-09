452a3486aa43946dc39bef63c7a7aab8a65fb1fb
otaviopetry <otaviopetry@gmail.com>
2024-06-09 20:52:48 -0300

    feat: debugging with Node REPL

---
+++ modulo10/01-repl/index.mjs
@@ -0,0 +1,20 @@
+import http from "http";
+
+function netSalary({ discount, salary }) {
+  const percent = discount / 100;
+  const cost = salary * percent;
+  const result = salary - cost;
+
+  return result;
+}
+
+http
+  .createServer((req, res) => {
+    const url = req.url.replace("/", "");
+    const params = new URLSearchParams(url);
+    const data = Object.fromEntries(params);
+    const result = netSalary(data);
+    debugger;
+    res.end(`O seu salário final é: ${result}`);
+  })
+  .listen(3000, () => console.log("App running at port 3000"));
